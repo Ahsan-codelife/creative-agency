11acc581283595c7658d34e08e109e546ebec963
Ahsan-codelife <65127583+Ahsan-codelife@users.noreply.github.com>
2020-10-17 16:11:28 +0600

    done

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCeJgRWrMZQ7SHZOltN59ntD35O-wJ0o44",
+  authDomain: "creative-agency-59ba5.firebaseapp.com",
+  databaseURL: "https://creative-agency-59ba5.firebaseio.com",
+  projectId: "creative-agency-59ba5",
+  storageBucket: "creative-agency-59ba5.appspot.com",
+  messagingSenderId: "303727162979",
+  appId: "1:303727162979:web:04266e0f3f02d299892a56"
+  };
+
+export default firebaseConfig;
